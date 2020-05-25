b0615639ee237f16bd24c65f8b8cee5e74a30dbd
Juan Gómez <gb.jc@outlook.com>
2020-05-25 15:35:17 -0600

    Clase 17: Limpieza del index.ts, el contenido anterior pasó a 02-unsubscribe-add.ts

--- Seccion_03_Observables/src/index.ts
+++ Seccion_03_Observables/src/index.ts
@@ -13,20 +13,26 @@ const intervalo$ = new Observable<number>((subs) => {
     subs.next(contador++);
   }, 1000);
 
+  setTimeout(() => {
+    subs.complete();
+  }, 2500);
+
   return () => {
     clearInterval(interval);
     console.log("Intérvalo destruido");
   };
 });
 
-const subs1 = intervalo$.subscribe((num) => console.log("Num1:", num));
-const subs2 = intervalo$.subscribe((num) => console.log("Num2:", num));
-const subs3 = intervalo$.subscribe((num) => console.log("Num3:", num));
+const subs1 = intervalo$.subscribe(observer);
+const subs2 = intervalo$.subscribe(observer);
+const subs3 = intervalo$.subscribe(observer);
+
+subs1.add(subs2).add(subs3);
 
 setTimeout(() => {
   subs1.unsubscribe();
-  subs2.unsubscribe();
-  subs3.unsubscribe();
+  //subs2.unsubscribe();
+  //subs3.unsubscribe();
 
   console.log("Completado Timeout");
-}, 3000);
+}, 6000);
