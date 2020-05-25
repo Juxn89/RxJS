360479f2e4d4dd5516390ed237d2aaf1c21e4877
Juan Gómez <gb.jc@outlook.com>
2020-05-25 15:03:47 -0600

    Clase 14: Otras maneras de implementar un subcribe

--- Seccion_03_Observables/src/index.ts
+++ Seccion_03_Observables/src/index.ts
@@ -1,6 +1,12 @@
-import { Observable } from "rxjs"; // para crear un observable
+import { Observable, observable, Observer } from "rxjs"; // para crear un observable
 
 //const obs$ = Observable.create();
+
+const observer: Observer<any> = {
+  next: (value) => console.log("Siguiente [next]: ", value),
+  error: (error) => console.warn("Siguiente [obs]: ", error),
+  complete: () => console.info("completado [obs]"),
+};
 
 /*
   Para que un observable se ejecute debe de terner una subscripción
@@ -12,6 +18,10 @@ const obs$ = new Observable<string>((subs) => {
   subs.next("Hola");
   subs.next("Mundo");
 
+  // Forzar un error
+  const a = undefined;
+  a.nombre = "Juan";
+
   subs.complete();
 
   /* Cuando se llama el complete, ninguna llamada despues del
@@ -21,4 +31,14 @@ const obs$ = new Observable<string>((subs) => {
   subs.next("Mundo");
 });
 
-obs$.subscribe(console.log);
+obs$.subscribe((resp) => {
+  console.log(resp);
+});
+
+obs$.subscribe(
+  (valor) => console.log("next: ", valor),
+  (error) => console.warn("error: ", error),
+  () => console.info("completado")
+);
+
+obs$.subscribe(observer);
