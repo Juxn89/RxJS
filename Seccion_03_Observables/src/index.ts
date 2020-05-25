e0b4a8f2d04710a0b52343d98eabaf3a8916dd20
Juan Gómez <gb.jc@outlook.com>
2020-05-25 15:27:25 -0600

    Clase 16: Implementación de subsciption y unsubcribe

--- Seccion_03_Observables/src/index.ts
+++ Seccion_03_Observables/src/index.ts
@@ -1,7 +1,32 @@
 import { Observable, observable, Observer } from "rxjs";
 
 const observer: Observer<any> = {
-  next: (value) => console.log("Siguiente [next]: ", value),
-  error: (error) => console.warn("Siguiente [obs]: ", error),
-  complete: () => console.info("completado [obs]"),
+  next: (value) => console.log("next: ", value),
+  error: (error) => console.warn("error: ", error),
+  complete: () => console.info("completado"),
 };
+
+const intervalo$ = new Observable<number>((subs) => {
+  let contador = 0;
+
+  const interval = setInterval(() => {
+    subs.next(contador++);
+  }, 1000);
+
+  return () => {
+    clearInterval(interval);
+    console.log("Intérvalo destruido");
+  };
+});
+
+const subs1 = intervalo$.subscribe((num) => console.log("Num1:", num));
+const subs2 = intervalo$.subscribe((num) => console.log("Num2:", num));
+const subs3 = intervalo$.subscribe((num) => console.log("Num3:", num));
+
+setTimeout(() => {
+  subs1.unsubscribe();
+  subs2.unsubscribe();
+  subs3.unsubscribe();
+
+  console.log("Completado Timeout");
+}, 3000);
