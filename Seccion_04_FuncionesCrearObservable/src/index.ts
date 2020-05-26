42c72c255c8123bab86c813bea2d7d2ebcf3302d
Juan Gómez <gb.jc@outlook.com>
2020-05-26 14:46:39 -0600

    Clase 24 of: Limpieza del index.ts, el código fuente se traslada a 04-of.ts

--- Seccion_04_FuncionesCrearObservable/src/index.ts
+++ Seccion_04_FuncionesCrearObservable/src/index.ts
@@ -1,10 +1,17 @@
+import { of } from "rxjs";
 
+//const obs$ = of(1, 2, 3, 4, 5, 6);
+//const obs$ = of(...[1, 2, 3, 4, 5, 6]);
+//const obs$ = of(...[1, 2, 3, 4, 5, 6], 2, 3, 4, 5);
+//const obs$ = of([1, 2, 3, 4, 5, 6]);
 
-console.log('Hola Mundo!');
+//const obs$ = of([1, 2], {a:1, b:2}, function(){}, true, Promise.resolve(true));
+const obs$ = of<number>(...[1, 2, 3, 4, 5, 6], 2, 6, 8, 9);
 
-
-
-
-
-
-
+//console.log("Inicio del Obs$");
+obs$.subscribe(
+  (next) => console.log("next: ", next),
+  null,
+  () => console.log("Terminamos la secuencia")
+);
+console.log("Fin del Obs$");
