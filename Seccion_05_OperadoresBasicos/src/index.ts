a046b6fa3f3228535a42fbaec6248ed5476f40a7
Juan Gómez <gb.jc@outlook.com>
2020-05-27 13:29:35 -0600

    Clase 34 map: Uso del operador map

--- Seccion_05_OperadoresBasicos/src/index.ts
+++ Seccion_05_OperadoresBasicos/src/index.ts
@@ -1,10 +1,16 @@
+import { range, fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
 
+//range(1, 5).subscribe((x) => console.log(x * 10));
+/*
+range(1, 5)
+  .pipe(
+    map<number, string>((val) => (val * 10).toString())
+  )
+  .subscribe(console.log);
+*/
 
-console.log('Hola Mundo!');
-
-
-
-
-
-
-
+const keyup$ = fromEvent<KeyboardEvent>(document, "keyup");
+//keyup$.pipe(map((event) => event.key));
+const keyupCode$ = keyup$.pipe(map((event) => event.key));
+keyupCode$.subscribe((key) => console.log("map: ", key));
