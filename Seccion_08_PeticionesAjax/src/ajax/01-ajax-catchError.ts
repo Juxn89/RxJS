20057bd4b9bf662146a308dfb6ae8f035fdb0996
Juan Gómez <gb.jc@outlook.com>
2020-06-01 13:56:58 -0600

    Clase 58 Peticiones usando ajax de RxJS - catchError

---
+++ Seccion_08_PeticionesAjax/src/ajax/01-ajax-catchError.ts
@@ -0,0 +1,34 @@
+const url = "https://api.github.com/usersXXX?per_page=5";
+
+const manejaErrores = (response: Response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response;
+};
+
+const fetchPromesa = fetch(url);
+/* fetchPromesa
+  .then((resp) => resp.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.warn("error en usuarios", err)); */
+
+/* fetchPromesa
+  .then(manejaErrores)
+  .then((resp) => resp.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.warn("error en usuarios", err)); */
+
+import { of } from "rxjs";
+import { ajax, AjaxError } from "rxjs/ajax";
+import { map, pluck, catchError } from "rxjs/operators";
+
+const atrapaError = (err: AjaxError) => {
+  console.warn("Error en:", err.message);
+  return of([]);
+};
+
+ajax(url)
+  .pipe(pluck("response"), catchError(atrapaError))
+  // map( (resp) => resp.response)
+  .subscribe((users) => console.log("Usuarios: ", users));
