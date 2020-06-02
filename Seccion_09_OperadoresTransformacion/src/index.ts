347880a6ecd25ccb7cc11ec4c0cada20550878e2
Juan Gómez <gb.jc@outlook.com>
2020-06-02 14:21:51 -0600

    Clase 75: Introducción al problema y necesidad de operadore de transfomación

--- Seccion_09_OperadoresTransformacion/src/index.ts
+++ Seccion_09_OperadoresTransformacion/src/index.ts
@@ -1,10 +1,23 @@
+import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, debounceTime, pluck } from "rxjs/operators";
 
+const body = document.querySelector("body");
+const textInput = document.createElement("input");
+const orderList = document.createElement("ol");
+body.append(textInput, orderList);
 
-console.log('Hola Mundo!');
+const input$ = fromEvent<KeyboardEvent>(document, "keyup");
 
-
-
-
-
-
-
+input$
+  .pipe(
+    debounceTime(500),
+    map((event) => {
+      const texto = event.target["value"];
+      // return texto;
+      return ajax.getJSON(`https://api.github.com/users/${texto}`);
+    })
+  )
+  .subscribe((resp) => {
+    resp.pipe(pluck("url")).subscribe(console.log);
+  });
